58902ce47fbc4f00135f37b6491a4ddeb0e73f9f
Christopher Stock <christopher.stock@mayflower.de>
2019-11-12 19:53:52 +0100

    Fixed all TSLinter warnings.

--- src/typescript/Main.ts
+++ src/typescript/Main.ts
@@ -1,5 +1,5 @@
 
-    import * as Matter from 'matter-js';
+    import * as matter from 'matter-js';
 
     /*******************************************************************************************************************
     *   The main class that launches the demo application.
@@ -11,12 +11,10 @@
         ***************************************************************************************************************/
         public run() : void
         {
-            // create engine
-            var engine = Matter.Engine.create();
-            var world  = engine.world;
-
-            // create renderer
-            var render = Matter.Render.create(
+            // create engine, world and renderer
+            const engine :matter.Engine = matter.Engine.create();
+            const world  :matter.World  = engine.world;
+            const render :matter.Render = matter.Render.create(
                 {
                     element: document.body,
                     engine: engine,
@@ -33,38 +31,39 @@
             );
 
             // run the renderer
-            Matter.Render.run( render );
+            matter.Render.run( render );
 
             // create runner
-            var runner = Matter.Runner.create( {} );
-            Matter.Runner.run(runner, engine);
+            const runner:matter.Runner = matter.Runner.create( {} );
+            matter.Runner.run( runner, engine );
 
             // add bodies
-            var group = Matter.Body.nextGroup(true);
-
-            var stack = Matter.Composites.stack( 250, 255, 1, 6, 0, 0, ( x:number, y:number ) :Matter.Body => {
-                return Matter.Bodies.rectangle(x, y, 30, 30);
-            });
-
-            var catapult = Matter.Bodies.rectangle(400, 520, 320, 20, {});
+            const group :number = matter.Body.nextGroup( true );
+            const stack :matter.Composite = matter.Composites.stack(
+                250, 255, 1, 6, 0, 0,
+                ( x:number, y:number ) :matter.Body => {
+                    return matter.Bodies.rectangle( x, y, 30, 30 );
+                }
+            );
+            const catapult :matter.Body = matter.Bodies.rectangle( 400, 520, 320, 20, {} );
 
             // add all bodies and constraints to the world
-            Matter.World.add( world, stack );
-            Matter.World.add( world, catapult );
-            Matter.World.add( world, Matter.Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true }) );
-            Matter.World.add( world, Matter.Bodies.rectangle(250, 555, 20, 50, { isStatic: true }) );
-            Matter.World.add( world, Matter.Bodies.rectangle(400, 535, 20, 80, { isStatic: true, collisionFilter: { group: group, category: 0, mask: 0 } }) );
-            Matter.World.add( world, Matter.Bodies.circle(560, 100, 50, { density: 0.005 }) );
-            Matter.World.add( world, Matter.Constraint.create({
+            matter.World.add( world, stack );
+            matter.World.add( world, catapult );
+            matter.World.add( world, matter.Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true }) );
+            matter.World.add( world, matter.Bodies.rectangle(250, 555, 20, 50, { isStatic: true }) );
+            matter.World.add( world, matter.Bodies.rectangle(400, 535, 20, 80, { isStatic: true, collisionFilter: { group: group, category: 0, mask: 0 } }) );
+            matter.World.add( world, matter.Bodies.circle(560, 100, 50, { density: 0.005 }) );
+            matter.World.add( world, matter.Constraint.create({
                 bodyA: catapult,
-                pointB: Matter.Vector.clone(catapult.position),
+                pointB: matter.Vector.clone(catapult.position),
                 stiffness: 1,
                 length: 0
             }) );
 
             // add mouse control
-            var mouse = Matter.Mouse.create(render.canvas);
-            var mouseConstraint = Matter.MouseConstraint.create(
+            const mouse           :matter.Mouse           = matter.Mouse.create( render.canvas );
+            const mouseConstraint :matter.MouseConstraint = matter.MouseConstraint.create(
                 engine,
                 {
                     mouse: mouse,
@@ -87,6 +86,6 @@
                     }
                 }
             );
-            Matter.World.add( world, mouseConstraint );
+            matter.World.add( world, mouseConstraint );
         }
     }
