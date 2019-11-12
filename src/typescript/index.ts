f58c3f24dbc995b078574c713b67a5429a8c0240
Christopher Stock <christopher.stock@mayflower.de>
2019-11-12 18:21:03 +0100

    Save working CatapultDemo sourcecode - original and ts.

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,19 +1,34 @@
 
-    import { MatterJsDemo } from './MatterJsDemo';
+    import * as Matter from 'matter-js';
+
+    // import { MatterJsDemo } from './MatterJsDemo';
+
+    require( './Demo.js' );
+
+
+
+//    import * as Example from './Demo.js';
+
+    //    import * as Demo from './Demo';
 
     /*******************************************************************************************************************
     *   Being invoked when the page is loaded completely.
     *******************************************************************************************************************/
+/*
     window.onload = () : void  =>
     {
         document.title = 'TypeScriptPrimer';
 
-        MatterJsDemo.run();
-    };
+        // MatterJsDemo.run();
 
+        Example.catapult();
+    };
+*/
     /*******************************************************************************************************************
     *   Being invoked when the page is left.
     *******************************************************************************************************************/
+/*
     window.onunload = () : void  =>
     {
     };
+*/
